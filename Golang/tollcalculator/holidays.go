589955fa15ce794986c3b0232c61e002fb3922a8
Mikael Jönsson <mikael.jonsson@unifaun.com>
2020-06-20 18:20:30 +0200

    Simpler better types

--- Golang/tollcalculator/holidays.go
+++ Golang/tollcalculator/holidays.go
@@ -6,13 +6,9 @@ package tollcalculator
  */
 
 // Holidays contains a list of all days of a year that are holidays
-type Holidays struct {
-	DaysOfYear []int
-}
+type Holidays []int
 
-var holidays2018 = Holidays{
-	[]int{1, 6, 89, 91, 92, 121, 130, 140, 157, 174, 307, 359, 360},
-}
+var holidays2018 = []int{1, 6, 89, 91, 92, 121, 130, 140, 157, 174, 307, 359, 360}
 
 // HolidaysForYear return holidays for the specific year
 func HolidaysForYear(year int) Holidays {
